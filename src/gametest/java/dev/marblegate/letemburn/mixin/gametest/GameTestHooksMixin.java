ae514365f96845dbe7be2245c571a9e0e918c3b1
MarbleGate <marblegatekeeper@outlook.com>
2026-08-13 13:19:34 +0800

    Enable GameTests in the development client

---
+++ src/gametest/java/dev/marblegate/letemburn/mixin/gametest/GameTestHooksMixin.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2026 MarbleGate
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package dev.marblegate.letemburn.mixin.gametest;
+
+import net.neoforged.neoforge.gametest.GameTestHooks;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(value = GameTestHooks.class, remap = false)
+public abstract class GameTestHooksMixin {
+    @Inject(method = "isGametestServer", at = @At("HEAD"), cancellable = true)
+    private static void letemburn$recognizeDevelopmentClient(CallbackInfoReturnable<Boolean> cir) {
+        cir.setReturnValue(true);
+    }
+}
